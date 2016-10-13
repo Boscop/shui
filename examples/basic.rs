f757e679570f15b470d7689efaf461c85ef73471
Boscop <kingboscop@gmail.com>
2016-10-13 20:51:13 +0200

    focus, key events, nested layouts

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,56 +1,96 @@
+#![allow(unused_imports)]
+#![feature(box_syntax)]
+
 use std::thread;
 use std::time::Duration;
+use std::cell::RefCell;
 
 extern crate shui;
 use shui::*;
 
 #[macro_use] extern crate glium;
-use glium::glutin::{ElementState, MouseButton};
+use glium::glutin::{ElementState, MouseButton, VirtualKeyCode};
 
+extern crate nalgebra as na;
+use na::{Vector2, RotationTo, Norm, Dot, Rotation2, Vector1, Vector3, zero, one};
 
 fn main() {
 	let mut gui = Gui::new();
-	//let layout = Layout::new(vec![1., 1., 2.]); // 1/4, 1/4, 1/2
-	//let fract = layout.fract();
-	let mut lay_x = LayX::new();
-	let button0 = lay_x.add(1.);
-	let button1 = lay_x.add(1.);
-	let button2 = lay_x.add(2.);
+	let mut lay_y = Layout::new(Axis::Y); //, 0);
+	let mut _lay_x0_id = lay_y.add(1.);
+	let mut _lay_x1_id = lay_y.add(1.);
+	let mut lay_x0 = Layout::new(Axis::X); //, lay_y.add(1.));
+	let mut lay_x1 = Layout::new(Axis::X); //, lay_y.add(1.));
+	let _button00 = lay_x0.add(1.);
+	let _button01 = lay_x0.add(1.);
+	let _button02 = lay_x0.add(2.);
+	let _button10 = lay_x1.add(1.);
+	let _button11 = lay_x1.add(1.);
 	let mut total_button_presses = 0;
-	while let Some(events) = gui.frame() {
+	let cell = RefCell::new(&mut total_button_presses);
+	'out: while let Some(events) = gui.frame() {
+		let parent_size: V = one();
 		thread::sleep(Duration::from_millis(20));
-		lay_x.take_events_for_children(events);
-		for &button_id in &[button0, button1, button2] {
-			lay_x.events_from_children.extend(lay_x.events_for_children[button_id].drain(..).filter(|ev| {
-				!match *ev {
-					GEvent(MouseInput(ElementState::Pressed, MouseButton::Left)) => {
-						println!("button{} clicked!", button_id);
-						total_button_presses += 1;
-						true
-					}
-					GEvent(Focused(focused)) => {
-						println!("button{} focus: {}", button_id, focused);
-						true
-					}
-					_ => false
+		for leftover_event in lay_y.events_for_children(events).into_iter().enumerate().flat_map(|(lay_x_id, events)| {
+			//let lay_x0 = &mut lay_x0;
+			let parent_size = lay_y.child_size(parent_size, lay_x_id);
+			let r = [
+				//(&mut |events| {
+				(box |events| {
+					//let lay_x0 = &mut *lay_x0;
+					let r = lay_x0.events_for_children(events).into_iter().enumerate().flat_map(|(button_id, events)| {
+						let parent_size = lay_x0.child_size(parent_size, button_id);
+						events.into_iter().filter(|ev| {
+							!match *ev {
+								GEvent(MouseInput(ElementState::Pressed, MouseButton::Left)) => {
+									println!("button0{} clicked!", button_id);
+									**cell.borrow_mut() += 1;
+									println!("total_button_presses {}", **cell.borrow_mut());
+									println!("pos size {}", parent_size);
+									true
+								}
+								Focus(p) => {
+									println!("button0{} focus: {:?}", button_id, p);
+									true
+								}
+								_ => false
+							}
+						}).collect::<Vec<_>>()
+					}).collect();
+					r
+				//}) as &mut FnMut(Vec<MyEvent>) -> Vec<MyEvent>,
+				}) as Box<FnMut(Vec<MyEvent>) -> Vec<MyEvent>>,
+				//&mut |events| {
+				box |events| {
+					let r = lay_x1.events_for_children(events).into_iter().enumerate().flat_map(|(button_id, events)| {
+						let parent_size = lay_x1.child_size(parent_size, button_id);
+						events.into_iter().filter(|ev| {
+							!match *ev {
+								GEvent(MouseInput(ElementState::Pressed, MouseButton::Left)) => {
+									println!("button1{} clicked!", button_id);
+									**cell.borrow_mut() += 1;
+									println!("total_button_presses {}", **cell.borrow_mut());
+									println!("pos size {}", parent_size);
+									true
+								}
+								Focus(p) => {
+									println!("button1{} focus: {:?}", button_id, p);
+									true
+								}
+								_ => false
+							}
+						}).collect::<Vec<_>>()
+					}).collect();
+					r
 				}
-			}));
-		}
-		gui.events_from_children.extend(lay_x.events_from_children.drain(..).filter(|ev| {
-			!match *ev {
-				MousePos(p) => {
-					println!("mouse pos {:?}", p);
-					true
-				}
-				GEvent(Focused(focused)) => {
-					println!("lay_x focus: {}", focused);
-					true
-				}
-				_ => false
+			][lay_x_id](events);
+			r
+		}).collect::<Vec<_>>() {
+			match leftover_event {
+				GEvent(KeyboardInput(ElementState::Released, _, Some(VirtualKeyCode::Escape))) => break 'out,
+				_ => ()
 			}
-		}));
-		for ev in gui.events_from_children.drain(..) {
-			println!("unused event: {:?}", ev);
+			println!("leftover_event: {:?}", leftover_event);
 		}
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@ use na::{Vector2, RotationTo, Norm, Dot, Rotation2, Vector1, Vector3, zero, one}
 extern crate asprim;
 use asprim::AsPrim;
 
+extern crate num;
+use num::{Float, NumCast, PrimInt, ToPrimitive};
+
 macro_rules! logit {
 	($($args:tt)*) => {
 		/*if /*cfg!(debug_assertions)*/false {
@@ -22,12 +25,13 @@ macro_rules! logit {
 	}
 }
 
-type V = Vector2<f32>;
+pub type V = Vector2<f32>;
 
 #[derive(Debug, Clone)]
 pub enum MyEvent {
 	GEvent(Event),
 	MousePos(V),
+	Focus(Option<V>),
 }
 pub use self::MyEvent::*;
 pub use glium::glutin::Event::*;
@@ -36,7 +40,7 @@ pub struct Gui {
 	display: Display,
 	window_size: V,
 	mouse_pos: V,
-	pub events_from_children: Vec<MyEvent>,
+	//pub events_from_children: Vec<MyEvent>,
 }
 impl Gui {
 	pub fn new(/*parent: *mut c_void*/) -> Gui {
@@ -46,7 +50,7 @@ impl Gui {
 			window_size: V::new(w.as_(), h.as_()),
 			display: display,
 			mouse_pos: zero(),
-			events_from_children: vec![],
+			//events_from_children: vec![],
 		}
 	}
 	pub fn frame(&mut self) -> Option<Vec<MyEvent>> {
@@ -58,7 +62,7 @@ impl Gui {
 					logit!("window closed");
 					return None;
 				}
-				Resized(_, _) => {
+				Resized(_w, _h) => {
 					let (w, h) = self.display.get_framebuffer_dimensions();
 					self.window_size = V::new(w.as_(), h.as_());
 				}
@@ -66,11 +70,12 @@ impl Gui {
 					self.mouse_pos = V::new(px.as_(), self.window_size.y - 1. - py as f32) / self.window_size;
 					events.push(MousePos(self.mouse_pos));
 				}
-				MouseInput(_, _) => events.push(GEvent(event)),
+				MouseInput(_x, _y) => events.push(GEvent(event)),
 				/*MouseWheel(MouseScrollDelta::LineDelta(px, py), _touchphase) => {
-				}
-				KeyboardInput(state, scan_code, Some(key)) =>*/
-				Focused(_) => events.push(GEvent(event)),
+				}*/
+				KeyboardInput(_state, _scan_code, _maybe_key) => events.push(GEvent(event)),
+				Focused(true) => events.push(Focus(Some(self.mouse_pos))),
+				Focused(false) => events.push(Focus(None)),
 				_ => ()
 			}
 		}
@@ -101,67 +106,145 @@ pub type WeightLayout = Vec<f32>;
 	events: Vec<Event>,
 }*/
 
-pub struct LayX {
+#[derive(Debug, Copy, Clone)]
+pub enum Axis {
+	X,
+	Y,
+	//Z,
+}
+
+pub type ChildId = usize;
+
+#[derive(Debug)]
+pub struct Layout {
+	axis: Axis,
 	layout_weight: WeightLayout,
 	layout_fract: FractLayout,
-	size: V,
+	//size: V,
 	//child_data: Vec<ChildData>
-	mouse_x: f32,
-	pub events_for_children: Vec<Vec<MyEvent>>,
-	pub events_from_children: Vec<MyEvent>,
-	focused: Option<usize>,
+	//mouse_x: f32,
+	//pub events_for_children: Vec<Vec<MyEvent>>,
+	//pub events_from_children: Vec<MyEvent>,
+	focused: Option<ChildId>,
 }
-impl LayX {
-	pub fn new() -> LayX {
-		LayX {
+impl Layout {
+	pub fn new(axis: Axis) -> Layout {
+		Layout {
+			axis: axis,
 			layout_weight: vec![],
 			layout_fract: vec![],
-			size: one(),
-			mouse_x: zero(),
-			events_for_children: vec![],
-			events_from_children: vec![],
+			//size: one(),
+			//mouse_x: zero(),
+			//events_for_children: vec![],
+			//events_from_children: vec![],
 			focused: None,
 		}
 	}
-	pub fn add(&mut self, weight: f32) -> usize {
+	pub fn add(&mut self, weight: f32) -> ChildId {
+		assert!(weight > 0.);
 		let id = self.layout_weight.len();
 		self.layout_weight.push(weight);
 		let total: f32 = self.layout_weight.iter().sum();
 		self.layout_fract = self.layout_weight.iter().map(|x| x / total).fold((vec![], 0.), |(mut v, a), x| {let a = a+x; v.push(a); (v, a)}).0;
-		self.events_for_children.push(vec![]);
+		//self.events_for_children.push(vec![]);
+		println!("{:?}", self);
 		id
 	}
-	pub fn take_events_for_children(&mut self, events: Vec<MyEvent>) {
-		for v in &mut self.events_for_children {
-			v.clear();
-		}
+	pub fn events_for_children(&mut self, events: Vec<MyEvent>) -> Vec<Vec<MyEvent>> {
+		let mut events_for_children = vec![vec![]; self.child_count()];
 		for ev in events {
 			match ev {
 				MousePos(p) => {
-					self.mouse_x = p.x;
-					let focused = self.layout_fract.iter().position(|&e| p.x < e).unwrap();
-					if let Some(self_focused) = self.focused {
+					let focused = self.child_at(p);
+					let self_focused = self.focused;
+					self.focused = Some(focused);
+					let p = self.pos_in_focused_child_area(p);
+					if let Some(self_focused) = self_focused {
 						if focused != self_focused {
-							self.events_for_children[self_focused].push(GEvent(Focused(false)));
-							self.events_for_children[focused].push(GEvent(Focused(true)));
+							events_for_children[self_focused].push(Focus(None));
+							events_for_children[focused].push(Focus(Some(p)));
 						}
 					}
-					self.focused = Some(focused);
-					let (l, r) = (if focused == 0 {0.} else {self.layout_fract[focused-1]}, self.layout_fract[focused]);
-					let mouse_x_rel = (p.x - l) / (r - l);
-					let pos = V::new(mouse_x_rel, p.y);
-					self.events_for_children[focused].push(MousePos(pos));
+					events_for_children[focused].push(MousePos(p));
 				}
+				GEvent(KeyboardInput(_, _, _)) |
 				GEvent(MouseInput(_, _)) => if let Some(self_focused) = self.focused {
-					self.events_for_children[self_focused].push(ev);
+					events_for_children[self_focused].push(ev);
 				},
-				GEvent(Focused(false)) => if let Some(self_focused) = self.focused {
-					self.events_for_children[self_focused].push(ev);
-				},
-				_ => (),
+				Focus(None) => {
+					if let Some(self_focused) = self.focused {
+						events_for_children[self_focused].push(ev);
+					}
+					self.focused = None;
+				}
+				Focus(Some(p)) => {
+					//println!("{:?}", self.axis);
+					assert!(self.focused.is_none());
+					let focused = self.child_at(p);
+					self.focused = Some(focused);
+					let p = self.pos_in_focused_child_area(p);
+					events_for_children[focused].push(Focus(Some(p)));
+				}
+				_ => ()
 			}
 		}
-
+		events_for_children
+	}
+	fn child_at(&self, p: V) -> ChildId {
+		let pos = match self.axis {
+			Axis::X => p.x,
+			Axis::Y => p.y,
+		};
+		self.layout_fract.iter().position(|&e| pos < e).unwrap()
+	}
+	pub fn pos_in_focused_child_area(&self, p: V) -> V {
+		let (a, b) = self.child_boundaries(self.focused.unwrap());
+		match self.axis {
+			Axis::X => V::new(percentage(p.x, a, b), p.y),
+			Axis::Y => V::new(p.x, percentage(p.y, a, b)),
+		}
+	}
+	fn child_count(&self) -> usize {
+		self.layout_fract.len()
+	}
+	fn child_boundaries(&self, id: ChildId) -> (f32, f32) {
+		(if id == 0 {0.} else {self.layout_fract[id-1]}, self.layout_fract[id])
+	}
+	pub fn child_size(&self, size: V, id: ChildId) -> V {
+		let (a, b) = self.child_boundaries(id);
+		let d = b - a;
+		match self.axis {
+			Axis::X => V::new(d, size.y),
+			Axis::Y => V::new(size.x, d),
+		}
 	}
 }
 
+pub fn percentage<T: Float + NumCast>(value: T, min: T, max: T) -> f32 {
+	let v: f32 = NumCast::from(value).unwrap();
+	let mn: f32 = NumCast::from(min).unwrap();
+	let mx: f32 = NumCast::from(max).unwrap();
+	(v - mn) / (mx - mn)
+}
+
+/*
+let v = vec![1,2,3];
+assert_eq!(v.clone(), [
+	Box::new(|e: Vec<usize>| Box::new(e.into_iter().flat_map(|x|vec![x])) as Box<Iterator<Item=usize>>)
+	as Box<FnMut(Vec<usize>) -> Box<Iterator<Item=usize>>>,
+	Box::new(|e: Vec<usize>| Box::new(e.into_iter().flat_map(|x|vec![x])) as Box<Iterator<Item=usize>>)
+	as Box<FnMut(Vec<usize>) -> Box<Iterator<Item=usize>>>,
+][1](v).collect::<Vec<_>>());
+
+({
+	let v = vec![1,2,3];
+	assert_eq!(v, [
+		&mut (
+			|e: Vec<usize>| e.into_iter().flat_map(|x| vec![x]).collect::<Vec<_>>()
+		) as &mut FnMut(Vec<usize>) -> Vec<usize>,
+		&mut (
+			|e: Vec<usize>| e.into_iter().flat_map(|x| vec![x]).collect::<Vec<_>>()
+		) as &mut FnMut(Vec<usize>) -> Vec<usize>,
+	][1](vec![1,2,3]));
+});
+*/
